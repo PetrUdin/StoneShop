5993b7c5425c44ba175c525e533298629df1347f
Пётр Юдин <udin.petr@gmail.com>
2021-03-16 11:26:52 +0300

    Add files via upload

---
+++ assets/script/module_ribbon/categories.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: '',
+    name: 'Все'
+  },
+  {
+    id: 'disign',
+    name: 'Ландшафтный дизайн'
+  },
+  {
+    id: 'track',
+    name: 'Мощение'
+  },
+  {
+    id: 'cladding',
+    name: 'Облицовка'
+  },
+  {
+    id: 'inner',
+    name: 'Для внутренних работ'
+  },
+];
